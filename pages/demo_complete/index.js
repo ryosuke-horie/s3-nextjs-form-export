df44e69fcd28fd6cde1e7e976ce6d0058a595d8e
ryosuke-horie <ryosuke03140@gmial.com>
2023-09-25 01:04:07 +0900

    :sparkles: 完了画面のデモを画像で用意

--- pages/demo_complete/index.js
+++ pages/demo_complete/index.js
@@ -2,25 +2,24 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Demo() {
+export default function DemoComplete() {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-6">
             <div className="mb-6">
                 {/* モバイル用の画像 */}
                 <div className="block md:hidden">
-                    <Image src="/stripe-mobile.jpg" alt="Stripe for Mobile" width={375} height={812} />
+                    <Image src="/stripe-complete-mobile.jpg" alt="Stripe for Mobile" width={375} height={812} />
                 </div>
 
                 {/* デスクトップ用の画像 */}
                 <div className="hidden md:block">
-                    <Image src="/stripe-desktop.jpg" alt="Stripe for Desktop" width={1440} height={900} />
+                    <Image src="/stripe-complete-desktop.jpg" alt="Stripe for Desktop" width={1440} height={900} />
                 </div>
             </div>
 
-            {/* /demo_completeへのリンクを追加 */}
-            <Link href="/demo_complete">
+            <Link href="/demo">
                 <div className="cursor-pointer px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-offset-2">
-                    完了へ進む
+                    お支払い画面へ戻る
                 </div>
             </Link>
         </div>
